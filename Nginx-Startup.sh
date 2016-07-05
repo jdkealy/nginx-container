2541695f0f6f6fb8024eeccb3ca631732089614b
Your Name <you@example.com>
2016-07-05 00:37:52 -0400

    m

--- Nginx-Startup.sh
+++ Nginx-Startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/APP_IP/'"$API_PORT_3001_TCP_ADDR"'/g' /etc/nginx/sites-enabled/default
-sed -i 's/APP_IP/'"$APP_PORT_3002_TCP_ADDR"'/g' /etc/nginx/sites-enabled/default
+sed -i 's/APP_IP/'"$API_PORT_3000_TCP_ADDR"'/g' /etc/nginx/sites-enabled/default
+sed -i 's/APP_IP/'"$APP_PORT_3001_TCP_ADDR"'/g' /etc/nginx/sites-enabled/default
 
 service nginx start
