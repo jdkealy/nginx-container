85f9c17efce5309b4afca03f1eb3a8da4a181ba3
Your Name <you@example.com>
2016-07-08 01:06:42 -0400

    test

--- Nginx-Startup.sh
+++ Nginx-Startup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 env
 printenv
+echo "PORT"
 echo  $API_PORT_3000_TCP_ADDR
+echo "PORT"
 sed -i 's/APP_IP/'"$APP_PORT_3000_TCP_ADDR"'/g' /etc/nginx/sites-enabled/api.myapp.com
+echo "PORT"
+echo  $API_PORT_3000_TCP_ADDR
+echo "PORT"
+
+echo "START"
 
 service nginx start
